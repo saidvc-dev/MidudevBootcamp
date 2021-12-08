2a5548fdd2eff83f3ac4ecb347800d5e7f9edb2f
Said Valencia Castrillo <50384031+said05@users.noreply.github.com>
2021-12-08 12:35:23 -0500

    uploading react project

---
+++ part0/script.js
@@ -0,0 +1,22 @@
+//variables and types
+let name = "Miguel"
+const number = 4
+var isDeveloper = true
+const list = []
+const person = {
+    name: "Miguel",
+    twitter: "miguel12",
+    age: 28,
+    isDeveloper: true,
+    links: ["http://miguel.com"]
+}
+
+//Expresion function
+const addition = (a, b) => {
+    return a + b
+}
+
+//Declaration function
+function substract(a, b) {
+    return a - b;
+}
